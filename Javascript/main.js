12e0b6ca15f79394c0bb8c773934df41b8f27f68
tjrans9248 <a900906@naver.com>
2022-12-20 14:48:27 +0900

    기능 연습

--- Javascript/main.js
+++ Javascript/main.js
@@ -3,6 +3,24 @@
 // 모든 div에 붙은 show 클래스명 제거
 // div 0에 show 클래스명 추가
 
+// var car2 = { name: '소나타', price: [50000, 3000, 4000] };
+
+// document.querySelector('.name').innerHTML = car2.name;
+// document.querySelector('.price').innerHTML = car2.price[1];
+
+// let products = [
+//   { id: 0, price: 70000, title: 'Blossom Dress' },
+//   { id: 1, price: 50000, title: 'Springfield Shirt' },
+//   { id: 2, price: 60000, title: 'Black Monastery' },
+// ];
+
+// document.querySelector('.title0').innerHTML = products[0].title;
+// document.querySelector('.title1').innerHTML = products[1].title;
+// document.querySelector('.title2').innerHTML = products[2].title;
+// document.querySelector('.price0').innerHTML = '가격 : ' + products[0].price;
+// document.querySelector('.price1').innerHTML = '가격 : ' + products[1].price;
+// document.querySelector('.price2').innerHTML = '가격 : ' + products[2].price;
+
 for (let i = 0; i < $('.tab-button').length; i++) {
   $('.tab-button')
     .eq(i)
@@ -40,4 +58,26 @@ for (let i = 0; i < $('.tab-button').length; i++) {
 //     $('.tab-button').eq(2).addClass('orange');
 //     $('.tab-content').removeClass('show');
 //     $('.tab-content').eq(2).addClass('show');
+//   });
+
+document
+  .querySelectorAll('.form-select')[0]
+  .addEventListener('input', function (e) {
+    var value = e.currentTarget.value;
+
+    if (value === '셔츠') {
+      document.querySelectorAll('.form-select')[1].classList.remove('hide');
+    } else {
+      document.querySelectorAll('.form-select')[1].classList.add('hide');
+    }
+  });
+
+// $('.form-select')
+//   .eq(0)
+//   .on('input', function () {
+//     var value = $('.form-select').eq(0).val();
+
+//     if (value === '셔츠') {
+//       $('.form-select').eq(1).removeClass('hide');
+//     }
 //   });
